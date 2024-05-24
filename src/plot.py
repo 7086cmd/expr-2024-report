c1bf9c361e0e385f4a1d575e21e7be1b4b34a166
7086cmd <54303040+7086cmd@users.noreply.github.com>
2024-05-24 17:09:35 +0800

    feat: data visualization for one example and the introductions

---
+++ src/plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data/2024-05-17(16-56-33).csv')
+
+plt.scatter(df['x'], df['y'], label='tag')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Trace of the tag')
+plt.legend()
+plt.savefig('figures/plot1.eps')
